84dd7d1d789ac38a6aa66d4446ff679331c4d2a2
taku-a <tky.aikawa@gmail.com>
2022-12-17 23:29:22 +0900

    【修正】NewSession、GetSessionのテストメソッドを作成

--- cmd/util/redis_test.go
+++ cmd/util/redis_test.go
@@ -1,44 +1,88 @@
 package util
 
 import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/alicebob/miniredis"
 	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
 )
 
-// 初期処理_テスト用redisサーバ作成
-func NewMockRedis(t *testing.T) *redis.Client {
-	t.Helper()
+var redisServer *miniredis.Miniredis
 
-	// redisサーバ作成
+func mockRedis() *miniredis.Miniredis {
 	s, err := miniredis.Run()
+
 	if err != nil {
-		t.Fatalf("unexpected error while createing test redis server '%#v'", err)
+		panic(err)
 	}
 
-	// *redis.Clientを用意
-	conn := redis.NewClient(&redis.Options{
-		Addr:     s.Addr(),
-		Password: "",
-		DB:       0,
+	return s
+}
+
+func setup() {
+	redisServer = mockRedis()
+	conn = redis.NewClient(&redis.Options{
+		Addr: redisServer.Addr(),
 	})
-	return conn
 }
 
-// 正常系
+func teardown() {
+	redisServer.Close()
+}
+
+// 正常系（セッション生成）
 func TestNewSession_Normal(t *testing.T) {
+	// miniredis初期化
+	setup()
+	defer teardown()
 
+	// httptest初期化
+	reqBody := bytes.NewBufferString("request body")
+	r := httptest.NewRequest(http.MethodGet, "http://dummy.url.com/user", reqBody)
+	w := httptest.NewRecorder()
+
+	err := NewSession(w, r, "test", "test")
+	if err != nil {
+		fmt.Println(err.Error())
+		t.Errorf("HTTP status code:%d", w.Code)
+	}
+	assert.Nil(t, err)
 }
 
-// 異常系
-func TestNewSession_Abnormal(t *testing.T) {
-
-}
-
-// 正常系
+// 正常系（セッション取得)
 func TestGetSession_Normal(t *testing.T) {
+	// miniredis初期化
+	setup()
+	defer teardown()
 
+	// httptest初期化
+	reqBody := bytes.NewBufferString("request body")
+	r := httptest.NewRequest(http.MethodGet, "http://dummy.url.com/user", reqBody)
+
+	// rediskeyを生成
+	b := make([]byte, 64)
+	newRedisKey := base64.URLEncoding.EncodeToString(b)
+
+	// Cookieを登録
+	r.AddCookie(&http.Cookie{
+		Name:  "cookieKey",
+		Value: newRedisKey,
+	})
+
+	// セッションを登録
+	conn.Set(r.Context(), newRedisKey, "redisValueTest", 0)
+
+	rtn, err := GetSession(r, "cookieKey")
+	if err != nil {
+		fmt.Println(err)
+	}
+	assert.Equal(t, "redisValueTest", rtn)
 }
 
 // 異常系
