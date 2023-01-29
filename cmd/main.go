557c2b294c88d89427e9e1f533eb4a245d02b8ea
taku-a <tky.aikawa@gmail.com>
2023-01-29 14:43:27 +0900

    【修正】ポート番号変更

--- cmd/main.go
+++ cmd/main.go
@@ -28,7 +28,7 @@ func main() {
 	http.HandleFunc("/favicon.ico", faviconHandler)
 
 	// ListenAndServe
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
